82fc4c2ff65950b9d87c70efcc3e832a2edc220e
Thomas Heck <t@b128.net>
2015-04-03 12:39:16 +0200

    cleanup

---
+++ container-main/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar target/commafeed.jar server /data/config.yml
--- files/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f /data/config.yml ]; then
-	cp /opt/config.yml /data/config.yml
-fi
-
-cd /opt/commafeed
-java -jar target/commafeed.jar server /data/config.yml
